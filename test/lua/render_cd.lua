fd426b25ebcab6b8659726d7ce18298850e353ec
scuri <scuri>
2009-06-13 20:38:51 +0000

    *** empty log message ***

--- test/lua/render_cd.lua
+++ test/lua/render_cd.lua
@@ -6,7 +6,11 @@ local image = im.ImageCreate(500, 500, im.RGB, im.BYTE)
 local canvas = image:cdCreateCanvas()  -- Creates a CD_IMAGERGB canvas
 
 canvas:Activate()  
+canvas:Background(cd.EncodeColor(255, 255, 255))
 canvas:Clear()
+fgcolor = cd.EncodeColor(255, 0, 0) -- red
+fgcolor = cd.EncodeAlpha(fgcolor, 50) -- semi transparent
+canvas:Foreground(fgcolor)
 canvas:Font("Times", cd.BOLD, 24)
 canvas:Text(100, 100, "Test")
 canvas:Line(0,0,100,100)
