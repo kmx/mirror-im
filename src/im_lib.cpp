da43b406cb10a9bc7f88ab1733bbf04ee0ff4916
scuri <scuri>
2010-06-22 13:26:00 +0000

    *** empty log message ***

--- src/im_lib.cpp
+++ src/im_lib.cpp
@@ -11,9 +11,9 @@
 #include "im_lib.h"
 
 /* This appears only here to avoid changing the im_lib.h header for bug fixes */
-#define IM_VERSION_FIX ".1"
-#define IM_VERSION_FIX_NUMBER 1
-#define IM_VERSION_FIX_DATE "2010/04/23"
+#define IM_VERSION_FIX ".2"
+#define IM_VERSION_FIX_NUMBER 2
+#define IM_VERSION_FIX_DATE "2010/06/22"
 
 static char *iVersion = "TECVERID.str:IM:LIB:" IM_VERSION IM_VERSION_FIX;
 
