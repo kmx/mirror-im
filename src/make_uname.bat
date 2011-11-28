3f58ce72b4ff495e96309941fd29e43c9685b1d7
scuri <scuri>
2011-11-28 18:40:24 +0000

    *** empty log message ***

--- src/make_uname.bat
+++ src/make_uname.bat
@@ -3,12 +3,14 @@ REM This builds all the libraries of the folder for 1 uname
 
 call tecmake %1 %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=im_process" %2 %3 %4 %5 %6 %7 %8
+call tecmake %1 "MF=im_process" "USE_OPENMP=Yes" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=im_jp2" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=im_avi" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=im_fftw" %2 %3 %4 %5 %6 %7 %8
 
 call tecmake %1 "MF=imlua5" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=imlua_process5" %2 %3 %4 %5 %6 %7 %8
+call tecmake %1 "MF=imlua_process5" "USE_OPENMP=Yes" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=imlua_jp2" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=imlua_avi" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=imlua_fftw5" %2 %3 %4 %5 %6 %7 %8
