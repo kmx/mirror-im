30db64e18ad6b6f784d73ad1f4f1ef3a1894da27
scuri <scuri>
2013-11-21 13:24:51 +0000

    version 3.8.2

--- src/im_lib.cpp
+++ src/im_lib.cpp
@@ -10,9 +10,9 @@
 #include "im_lib.h"
 
 /* This appears only here to avoid changing the im_lib.h header for bug fixes */
-#define IM_VERSION_FIX ".1"
-#define IM_VERSION_FIX_NUMBER 1
-#define IM_VERSION_FIX_DATE "2012/11/26"
+#define IM_VERSION_FIX ".2"
+#define IM_VERSION_FIX_NUMBER 2
+#define IM_VERSION_FIX_DATE "2013/11/21"
 
 static char *iVersion = "TECVERID.str:IM:LIB:" IM_VERSION IM_VERSION_FIX;
 
